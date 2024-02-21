37e2e0efc369997c8c3b215d47848159483b5761
Scharly <scharly.opitz@gmail.com>
2024-02-21 17:02:06 +0100

    Ajout data-type sur les boutons de filtre

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,13 +1,22 @@
 import allTypes from "../Types/Types.json";
 
 export default function Header() {
+  const filterPokemons = () => {
+    console.log("ok");
+  };
+
   return (
     <header>
       <h1>Pokedex</h1>
       <div className="btn-container">
         {allTypes.map((type, idx) => {
           return (
-            <button style={{ border: `2px solid ${type.color}` }} key={idx}>
+            <button
+              data-type={type.type}
+              onClick={filterPokemons}
+              style={{ border: `2px solid ${type.color}` }}
+              key={idx}
+            >
               {type.type}
             </button>
           );

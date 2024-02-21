22ccb6b734ed567223fae9b94c76f7ee0c27823a
Scharly <scharly.opitz@gmail.com>
2024-02-21 17:36:00 +0100

    Ajout class Active au click des boutons filtre

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,19 +1,26 @@
+import { useState } from "react";
 import allTypes from "../Types/Types.json";
 
 export default function Header() {
-  const filterPokemons = () => {
-    console.log("ok");
+  const [isActive, setIsActive] = useState("");
+
+  const filterPokemons = (e) => {
+    setIsActive(e.target.dataset.type);
   };
 
   return (
     <header>
       <h1>Pokedex</h1>
+
       <div className="btn-container">
         {allTypes.map((type, idx) => {
           return (
             <button
+              className={isActive === `${type.type}` ? "Active" : null}
               data-type={type.type}
-              onClick={filterPokemons}
+              onClick={(e) => {
+                filterPokemons(e);
+              }}
               style={{ border: `2px solid ${type.color}` }}
               key={idx}
             >

ae680a07835ebecbf4c92e7a06c8e04541dc2e52
Scharly <scharly.opitz@gmail.com>
2024-02-22 18:54:43 +0100

    Ajout BackgroundBis et changement fond des cartes

--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -3,6 +3,7 @@ import allTypes from "../Types/Types.json";
 
 export default function Card({ id, name, image, type }) {
   const [color, setColor] = useState();
+  console.log(color);
 
   const callColor = () => {
     allTypes.map((t) => type === `${t.type}` && setColor(`${t.color}`));
@@ -13,7 +14,10 @@ export default function Card({ id, name, image, type }) {
   }, [type]);
 
   return (
-    <div className="card" style={{ border: `3px solid ${color}` }}>
+    <div
+      className="card"
+      style={{ border: `3px solid ${color}`, background: `` }}
+    >
       <h1>#{id}</h1>
       <h1>{name}</h1>
       <img src={image} alt={name} />

316291aa22f01ada155ccbf5e8075b6e7a298e44
Scharly <scharly.opitz@gmail.com>
2024-02-24 12:40:28 +0100

    Création page PokemonStats

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import Card from "./assets/components/Card";
 import Home from "./assets/components/Home";
-
 import { Route, Routes } from "react-router";
+import PokemonStats from "./assets/components/PokemonStats";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/:name" element={<Card />} />
+      <Route path="/:name" element={<PokemonStats />} />
     </Routes>
   );
 }
---
+++ src/assets/components/PokemonStats.jsx
@@ -0,0 +1,3 @@
+export default function PokemonStats() {
+  return <h1>Coucou</h1>;
+}

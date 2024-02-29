047e51e529f57a1b1cd8a4813215c48cd2a7c850
Scharly <scharly.opitz@gmail.com>
2024-02-29 18:19:41 +0100

    Designe pokedex

--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -3,17 +3,7 @@ import PokeBall from "/pokeball.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretRight } from "@fortawesome/free-solid-svg-icons";
 
-export default function Card({ id, name, image, type, setSearchPokemon }) {
-  // const [color, setColor] = useState([]);
-
-  // const callColor = () => {
-  //   allTypes.map((t) => type === `${t.type}` && setColor(t.color));
-  // };
-
-  // useEffect(() => {
-  //   callColor();
-  // }, [type]);
-
+export default function Card({ id, name, image, setSearchPokemon }) {
   return (
     <>
       <Link
@@ -21,20 +11,10 @@ export default function Card({ id, name, image, type, setSearchPokemon }) {
         data-id={id}
         to={`/PokemonStats/${name}`}
         className="card"
-        // style={{
-        //   border: `3px solid ${color.border}`,
-        //   background: `${color.background}`,
-        //   boxShadow: `0px 0 30px 5px ${color.border},inset 0px 0 20px ${color.border}`,
-        // }}
       >
-        {/* <div
-        className="background"
-        style={{ background: `${color.background}` }}
-      ></div> */}
         <img className="pokeball" src={PokeBall} alt="Pokeball" />
         <img src={image} alt={name} />
         <p>N° {id}</p> <p>{name}</p>
-        {/* <p>Type : {type}</p> */}
         <div className="hoveredPokemon">
           <FontAwesomeIcon icon={faCaretRight} style={{ color: "#ffffff" }} />
         </div>

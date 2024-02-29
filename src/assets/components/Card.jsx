0578517bb4a13df6e9bb27932c72c463ba6fd0dd
Scharly <scharly.opitz@gmail.com>
2024-02-29 17:56:30 +0100

    Mise en place du designe pokedex

--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -1,37 +1,44 @@
-import { useEffect, useState } from "react";
-import allTypes from "../Types/Types.json";
 import { Link } from "react-router-dom";
+import PokeBall from "/pokeball.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretRight } from "@fortawesome/free-solid-svg-icons";
 
 export default function Card({ id, name, image, type, setSearchPokemon }) {
-  const [color, setColor] = useState([]);
+  // const [color, setColor] = useState([]);
 
-  const callColor = () => {
-    allTypes.map((t) => type === `${t.type}` && setColor(t.color));
-  };
+  // const callColor = () => {
+  //   allTypes.map((t) => type === `${t.type}` && setColor(t.color));
+  // };
 
-  useEffect(() => {
-    callColor();
-  }, [type]);
+  // useEffect(() => {
+  //   callColor();
+  // }, [type]);
 
   return (
-    <Link
-      onClick={() => setSearchPokemon("")}
-      data-id={id}
-      to={`/PokemonStats/${name}`}
-      className="card"
-      style={{
-        border: `3px solid ${color.border}`,
-        // background: `${color.background}`,
-        // boxShadow: `0px 0 30px 5px ${color.border},inset 0px 0 20px ${color.border}`,
-      }}
-    >
-      {/* <div
+    <>
+      <Link
+        onClick={() => setSearchPokemon("")}
+        data-id={id}
+        to={`/PokemonStats/${name}`}
+        className="card"
+        // style={{
+        //   border: `3px solid ${color.border}`,
+        //   background: `${color.background}`,
+        //   boxShadow: `0px 0 30px 5px ${color.border},inset 0px 0 20px ${color.border}`,
+        // }}
+      >
+        {/* <div
         className="background"
         style={{ background: `${color.background}` }}
       ></div> */}
-      <img src={image} alt={name} />
-      <p>N° {id}</p> <p>{name}</p>
-      {/* <p>Type : {type}</p> */}
-    </Link>
+        <img className="pokeball" src={PokeBall} alt="Pokeball" />
+        <img src={image} alt={name} />
+        <p>N° {id}</p> <p>{name}</p>
+        {/* <p>Type : {type}</p> */}
+        <div className="hoveredPokemon">
+          <FontAwesomeIcon icon={faCaretRight} style={{ color: "#ffffff" }} />
+        </div>
+      </Link>
+    </>
   );
 }

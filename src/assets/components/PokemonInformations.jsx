0f7b3d6ea6f2826d43f6e2d1f7772ba0fe25f97d
Scharly <scharly.opitz@gmail.com>
2024-03-05 20:33:58 +0100

    Design PokePage

--- src/assets/components/PokemonInformations.jsx
+++ src/assets/components/PokemonInformations.jsx
@@ -17,40 +17,49 @@ export default function PokemonInformations({
 
   return (
     <>
-      <div>
-        <h1>N° {PokemonId}</h1>
-        <h1>{PokemonName}</h1>
-        {PokemonImgShiny && (
-          <p>
-            version : <button onClick={() => setShiny(false)}>Normal</button>
-            {" | "}
-            <button onClick={() => setShiny(true)}>Shiny</button>
-          </p>
-        )}
+      <div className="information_container">
         <div className="currentPokemon">
           <img
             src={shiny ? PokemonImgShiny : PokemonImgRegular}
             alt={`image ${PokemonName}`}
           />
         </div>
-        <Evolutions
+        <div className="informations">
+          <h1>N° {PokemonId}</h1>
+          <h1>{PokemonName}</h1>
+          {PokemonImgShiny && (
+            <p>
+              version : <button onClick={() => setShiny(false)}>Normal</button>
+              {" | "}
+              <button onClick={() => setShiny(true)}>Shiny</button>
+            </p>
+          )}
+          {/* <Evolutions
           PreEvolutions={PreEvolutions}
           NextEvolutions={NextEvolutions}
           shiny={shiny}
-        />
-        <p>Taille : {PokemonHeight} </p>
-        <p>Catégorie : {PokemonCategory} </p>
-        <p>Poids : {PokemonWeight}</p>
-        Talent(s) :
-        {poke.talents?.map((talent, idx) => (
-          <p key={idx}>{talent.name}</p>
-        ))}
-        Type :
-        {poke.types?.map((type, idx) => (
-          <div key={idx}>
-            {type.name} <img src={type.image} alt={type.name} />
+        /> */}
+          Talent(s) :
+          {poke.talents?.map((talent, idx) => (
+            <p key={idx}>{talent.name}</p>
+          ))}
+          <p>{PokemonCategory} </p>
+          <div className="information">
+            <p className="vignette">Type</p>
+            {poke.types?.map((type, idx) => (
+              <div key={idx} className="type">
+                <img src={type.image} alt={type.name} />
+                <p>{type.name.toUpperCase()}</p>
+              </div>
+            ))}
           </div>
-        ))}
+          <div className="information">
+            <p className="vignette">Taille</p> {PokemonHeight}{" "}
+          </div>
+          <div className="information">
+            <p className="vignette">Poids</p> {PokemonWeight}
+          </div>
+        </div>
       </div>
     </>
   );

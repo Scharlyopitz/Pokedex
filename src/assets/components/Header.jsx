496a0717ed21b03c7ce79f15c5af69605eeca878
Scharly <scharly.opitz@gmail.com>
2024-03-11 19:21:10 +0100

    Design du header

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -8,13 +8,15 @@ export default function Header({
 }) {
   return (
     <header>
-      <h1>POKEDEX</h1>
-      <input
-        value={searchPokemon}
-        type="text"
-        placeholder="Recherche un Pokemon..."
-        onChange={(e) => setSearchPokemon(e.target.value.toLocaleLowerCase())}
-      />
+      <div>
+        <h1>POKEDEX</h1>
+        <input
+          value={searchPokemon}
+          type="text"
+          placeholder="Recherche un Pokemon..."
+          onChange={(e) => setSearchPokemon(e.target.value.toLocaleLowerCase())}
+        />
+      </div>
 
       <FilterButtons
         setPokemonCategory={setPokemonCategory}
